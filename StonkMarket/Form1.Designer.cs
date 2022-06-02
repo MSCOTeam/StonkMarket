440d1414da61cbe5dde2171ca8fa71c68a99f71d
Nika <mysummercaronline@gmail.com>
2022-06-02 10:02:04 +0100

    Latest Update

--- StonkMarket/Form1.Designer.cs
+++ StonkMarket/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // cartesianChart1
@@ -45,24 +50,77 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(240, 15);
+            this.label1.Location = new System.Drawing.Point(240, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(460, 93);
             this.label1.TabIndex = 1;
             this.label1.Text = "$0000.000";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 556);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "INCREASE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 556);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DECREASE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 556);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "HALT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(576, 552);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(585, 578);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SET VALUE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.ClientSize = new System.Drawing.Size(693, 536);
+            this.ClientSize = new System.Drawing.Size(693, 603);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cartesianChart1);
             this.Name = "Form1";
             this.Text = "t";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +128,11 @@
 
         private LiveCharts.WinForms.CartesianChart cartesianChart1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- StonkMarket/Form1.cs
+++ StonkMarket/Form1.cs
@@ -21,12 +21,21 @@ namespace StonkMarket
         private int chats = 0;
         private double tips = 0;
         private double bits = 0;
+
+        private int oldi = 0;
+
+        private bool HALT;
+        //private double progressiveChange;
+        //private bool isProgressive;
+
         public Form1()
         {
             InitializeComponent();
             Setup();
         }
-        
+
+        Func<double, string> formatFunc = (x) => string.Format("{0:0.00}", x);
+
         public void Setup()
         {
             var mapper = Mappers.Xy<MeasureModel>()
@@ -44,7 +53,7 @@ namespace StonkMarket
                 {
                     Values = ChartValues,
                     PointGeometrySize = 0,
-                    StrokeThickness = 5,
+                    StrokeThickness = 8,
                     LineSmoothness = 0.1,
                     Fill = System.Windows.Media.Brushes.Transparent,
                     Stroke = System.Windows.Media.Brushes.White
@@ -62,24 +71,26 @@ namespace StonkMarket
             });
             cartesianChart1.AxisY.Add(new Axis
             {
+                FontSize = 20,
+                LabelFormatter = formatFunc,
                 Separator = new Separator
                 {
                     StrokeThickness = 0
                 }
-            });
+            });;
 
             // Get Initial Values
-            bits = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_cheer_amount.txt"));
-            tips = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_donation_amount.txt").Split('$')[1]);
-            followers = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_follower_count.txt"));
-            subcount = int.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_subscriber_count.txt"));
+            bits = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_cheer_amount.txt"));
+            tips = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_donation_amount.txt").Split('$')[1]);
+            followers = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_follower_count.txt"));
+            subcount = int.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_subscriber_count.txt"));
 
             SetAxisLimits(DateTime.Now);
 
             //The next code simulates data changes every 500 ms
             Timer = new Timer
             {
-                Interval = 1000
+                Interval = 2000
             };
             Timer.Tick += TimerOnTick;
             R = new Random();
@@ -104,21 +115,21 @@ namespace StonkMarket
 
             // Followers and Subs
             // Get follower and sub count
-            double newFollowers = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_follower_count.txt"));
-            double newSubcount = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_subscriber_count.txt"));
-            double newBits = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_cheer_amount.txt"));
-            double newTips = double.Parse(File.ReadAllText("C:/Users/Nika/Desktop/Twitch Stuff/Labels/total_donation_amount.txt").Split('$')[1]);
+            double newFollowers = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_follower_count.txt"));
+            double newSubcount = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_subscriber_count.txt"));
+            double newBits = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_cheer_amount.txt"));
+            double newTips = double.Parse(File.ReadAllText("C:/Users/Nikat/Desktop/Twitch Stuff/Labels/total_donation_amount.txt").Split('$')[1]);
 
             // Apply changes according 
             if (newFollowers != followers)
             {
                 if (newFollowers > followers)
                 {
-                    stonkChange += newFollowers - followers;
+                    stonkChange += (newFollowers - followers) * 0.1;
                 }
                 else
                 {
-                    stonkChange += (newFollowers - followers);
+                    stonkChange += (newFollowers - followers) * 0.1;
                 }
 
                 followers = newFollowers;
@@ -127,38 +138,45 @@ namespace StonkMarket
             {
                 if (newSubcount > subcount)
                 {
-                    stonkChange += (newSubcount - subcount) * 5;
+                    stonkChange += (newSubcount - subcount) * 1;
                 }
 
                 subcount = newSubcount;
             }
             if (newBits > bits)
             {
-                stonkChange += (newBits - bits) / 50;
+                stonkChange += (newBits - bits) * 0.005;
                 bits = newBits;
             }
             if (newTips > tips)
             {
-                stonkChange += (newTips - tips) * 2;
+                stonkChange += (newTips - tips) * 0.5;
                 tips = newTips;
             }
 
             // Viewers and Chat
             // Get chat logs
-            using (var fs = new FileStream("C:/Users/Nika/Desktop/Twitch Stuff/StonkMarket/Data/#nixka.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var fs = new FileStream("C:/Users/Nikat/Desktop/Twitch Stuff/StonkMarket/Data/#nixka.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var sr = new StreamReader(fs, Encoding.Default))
             {
                 int i = 0;
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (line.Contains("VIEWERS"))
+                    if (line.Contains("VIEWERS") && i > oldi)
                     {
-                        int newViewers = int.Parse(line.Split(':')[2].Substring(1));
+                        int newViewers = int.Parse(line.Split(':')[1].Substring(1));
                         if (newViewers > viewers)
                         {
-                            stonkChange += (newViewers - viewers) * 0.2;
+                            stonkChange += (newViewers - viewers) * 0.1;
+                            //MessageBox.Show($"new {newViewers} and old {viewers}, stonkchange {stonkChange}, index {i}");
                         }
+                        else if (newViewers < viewers)
+                        {
+                            stonkChange -= (viewers - newViewers) * 0.05;
+                        }
+                        oldi = i;
+                        viewers = newViewers;
                     }
                     else 
                     { 
@@ -167,9 +185,10 @@ namespace StonkMarket
                 }
                 if (i > chats)
                 {
-                    stonkChange += (i- chats) * 0.01;
+                    stonkChange += (i- chats) * 0.005;
                     chats = i;
                 }
+                
             }
             /* 
              * CANT USE THIS METHOD WHEN TEXT FILE BEING USED BY OTHER PROCESS
@@ -192,8 +211,22 @@ namespace StonkMarket
                 chats = lines.Length;
             }*/
 
+            // Change label color if anything changed prior to auto adjustment
+            if (stonkChange > 0)
+            {
+                label1.ForeColor = System.Drawing.Color.Lime;
+            }
+            else if (stonkChange < 0)
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.White;
+            }
+
             // Randomizer (randomize stonkchange so its not a straight line)
-            if (stonkChange == 0)
+            if (stonkChange == 0 && !HALT)
             {
                 Random r = new Random();
                 int num = r.Next(0, 10);
@@ -206,8 +239,17 @@ namespace StonkMarket
                     stonkChange -= double.Parse($"0.00{r.Next(0, 10)}");
                 }
             }
+
+
+            /* Update stonk price progressively if high 
             
-            
+            if (stonkChange > 2) {
+                progressiveChange = stonkChange / 5;
+            }
+            else {
+                stonkPrice += stonkChange;
+            }*/
+
             // Update stonk price with changes
             stonkPrice += stonkChange;
             label1.Text = "$" + stonkPrice.ToString("0.00");
@@ -229,6 +271,26 @@ namespace StonkMarket
             
             // lets only use the last 30 values
             if (ChartValues.Count > 90) ChartValues.RemoveAt(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stonkChange += 0.5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stonkChange -= 0.5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HALT = !HALT;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stonkPrice = double.Parse(textBox1.Text);
         }
     }
 }
